9cd9a0a7f84c9ff8a682db538505a2f3f14b4043
Bhavesh patil <160614752+bhavesh21174@users.noreply.github.com>
2026-06-22 00:04:18 +0530

    Update db_conn.php

--- db_conn.php
+++ db_conn.php
@@ -1,5 +1,5 @@
 <?php
 
-$conn = mysqli_connect('localhost','u327703988_website_m_D_30','Website_m_D_30','u327703988_website_m_D_30') or die('connection failed');
+$conn = mysqli_connect('localhost','#','Website_m_D_30','#') or die('connection failed');
 
 ?>
